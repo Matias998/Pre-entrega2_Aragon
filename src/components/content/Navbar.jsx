2eadfe57cfd43c7335e9a28d1bffd26d27decf66
matiasAragon <m4tthe0.ar@gmail.com>
2022-10-06 04:29:15 -0300

    segunda pre-entrega del PF

---
+++ src/components/content/Navbar.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Pagelinks from '../layouts/Pagelinks.jsx';
+import Dropdown from '../layouts/Dropdown.jsx';
+import CartWidget from '../layouts/CartWidget.jsx';
+import SearchBar from '../layouts/SearchBar.jsx';
+
+const Navbar = () => {
+   const dropLista = ["Accion", "Disparos", "Deporte", "Rol", "Lucha"]
+
+   return (
+      <>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+         <div className="container-fluid">
+            <Link className="nav-link navbar-brand" to={'/'}>OP</Link>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+               <span className="navbar-toggler-icon" />
+            </button>
+            <div className="collapse navbar-collapse" id="navbarColor01">
+               <ul className="navbar-nav me-auto">
+                  <Pagelinks />
+                  <Dropdown lista={dropLista}/>
+               </ul>
+               <CartWidget />
+               <SearchBar busquedaPlaceholder="Buscar por nombre"/>
+            </div>
+         </div>
+      </nav>
+      </>
+   );
+}
+
+export default Navbar;
